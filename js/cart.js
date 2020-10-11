86c8c13878a586a94b4086d4be1965ce96f870a6
Rodrigo Figueredo <59716658+rgfigueredo91@users.noreply.github.com>
2020-10-11 13:30:46 -0300

    hjm

--- js/cart.js
+++ js/cart.js
@@ -2,5 +2,44 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(CART_INFO_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {   
+            carrito = resultObj.data;
+            mostrarArticulos(carrito.articles);
+        }
+        });
+    });
 
-});
+
+function subtotal(precioUnitario){
+    let cantidad = document.getElementById("cantidadProductos").value;
+    let subTotal = precioUnitario * cantidad;
+    document.getElementById("subtotal").innerHTML = subTotal;
+}
+
+function mostrarArticulos(array){
+    let contenido = "";
+    for(let i = 0; i < array.length; i++){
+     articulos = array[i];
+        contenido += `
+        <tr>
+        <td><img src="`+ articulos.src + `" width="45px"></td>
+        <td>`+ articulos.name +`</td>
+        <td>`+ articulos.currency +`</td>
+        <td>`+ articulos.unitCost +`</td>
+        <td><input type="number" name="productInput" id="cantidadProductos" value="`+ articulos.count +`" min="1"></td>
+        <td id="subtotal"></td>
+        </tr>
+        `
+        document.getElementById("tablacarrito").innerHTML = contenido;
+        }
+
+        precioUnitario = articulos.unitCost;
+        subtotal(precioUnitario);  
+        document.getElementById("cantidadProductos").addEventListener("change", function(){
+        subtotal(precioUnitario); 
+        });
+}
+
+
