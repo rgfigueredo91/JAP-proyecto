e251a287d5fa99f869e47ad8fb7a5a52e73434bb
Rodrigo Figueredo <59716658+rgfigueredo91@users.noreply.github.com>
2020-10-21 10:09:53 -0300

    asa

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,11 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+var precioTotal = document.getElementById("costoTotal");
+var totalProductos = document.getElementById("costoProducto");
+var gastoPorcentaje = document.getElementById("comision");
+
+
 document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(CART_INFO_URL).then(function(resultObj){
         if (resultObj.status === "ok")
@@ -11,12 +16,56 @@ document.addEventListener("DOMContentLoaded", function(e){
         });
     });
 
+function chequeoPorcentajes () {
+    let checkBox1 = document.getElementById("envio1");
+    let checkBox2 = document.getElementById("envio2");
+    let checkBox3 = document.getElementById("envio3");
+    if (checkBox1.checked == true){
+        return checkBox1.value}
+        if (checkBox2.checked == true){
+            return checkBox2.value
+}            if (checkBox3.checked == true){
+                return checkBox3.value
+}
+}
+
+
 
 function subtotal(precioUnitario){
+
     let cantidad = document.getElementById("cantidadProductos").value;
     let subTotal = precioUnitario * cantidad;
+
+
+
+    let porcentaje = subTotal * chequeoPorcentajes() / 100
+
+
+
+    let total = subTotal + porcentaje
+    document.getElementById("comision").innerHTML = porcentaje;
     document.getElementById("subtotal").innerHTML = subTotal;
+    document.getElementById("costoProducto").innerHTML = subTotal;
+    document.getElementById("costoTotal").innerHTML = total;
 }
+
+function actualizar(){
+    let cantidad = document.getElementById("cantidadProductos").value;
+    let subTotal = precioUnitario * cantidad;
+
+
+
+    let porcentaje = subTotal * chequeoPorcentajes() / 100
+
+
+
+    let total = subTotal + porcentaje
+    document.getElementById("comision").innerHTML = porcentaje;
+    document.getElementById("subtotal").innerHTML = subTotal;
+    document.getElementById("costoProducto").innerHTML = subTotal;
+    document.getElementById("costoTotal").innerHTML = total;
+}
+
 
 function mostrarArticulos(array){
     let contenido = "";
@@ -43,3 +92,12 @@ function mostrarArticulos(array){
 }
 
 
+
+function finish() {
+    if(document.getElementsByTagName("form")[0].checkValidity()){
+        alert("tu compra fue realizada con exito")
+    }
+    else {
+        alert("completa los datos")
+    }
+}
